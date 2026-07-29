66b3d46cf28cc8ab24b31f2f48e3dfeaee617bb2
Sheila Aparicio <sheylaapariciosuarez@gmail.com>
2026-07-30 01:04:23 +0200

    modified:   app/estimulacioncognitiva/envejecimiento/page.tsx

--- app/estimulacioncognitiva/envejecimiento/page.tsx
+++ app/estimulacioncognitiva/envejecimiento/page.tsx
@@ -9,13 +9,15 @@ export default function Envejecimiento() {
   
 <h2>¿Qué ofrecemos?</h2>
   <p>Estimulacion cognitiva y apoyo para la vida diaria para personas con deterioro cognitivo</p>
-  <p>Asesoramiento a padres y familias</p>
+  <p>Intervención en personas con daño cerebral adquirido u otras patologías neuológicas.</p>
+  <p>Asesoramiento a familias</p>
 
 <h2>Metodología</h2>
   <p>¿En que te ayudamos y como lo hacemos?</p>
-  <p> Enfermedad de Alzheimer y otras demencias</p>
+  <p>Enfermedad de Alzheimer y otras demencias</p>
   <p>Enfermedad de Parkinson</p>
   <p>Daño cerebral adquirido</p>
+  <p>Esclerosis múltiple</p>
 
           <Link href="/reservas?servicio=estimulacioncognitiva">
     <button className="boton-reservar">
