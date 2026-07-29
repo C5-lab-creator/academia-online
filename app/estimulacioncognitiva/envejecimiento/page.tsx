def7b42ffbc4eded7aaa6ad1922d266aabd5cd39
Sheila Aparicio <sheylaapariciosuarez@gmail.com>
2026-07-30 01:08:51 +0200

    modified:   app/estimulacioncognitiva/envejecimiento/page.tsx

--- app/estimulacioncognitiva/envejecimiento/page.tsx
+++ app/estimulacioncognitiva/envejecimiento/page.tsx
@@ -7,30 +7,30 @@ export default function Envejecimiento() {
 
 <div className="cursos-grid">
   
-<h2>¿Qué ofrecemos?</h2>
-  <p>Estimulacion cognitiva y apoyo para la vida diaria para personas con deterioro cognitivo</p>
-  <p>Intervención en personas con daño cerebral adquirido u otras patologías neuológicas.</p>
-  <p>Asesoramiento a familias</p>
+ <h2>¿Qué ofrecemos?</h2>
+   <p>Estimulacion cognitiva y apoyo para la vida diaria para personas con deterioro cognitivo</p>
+   <p>Intervención en personas con daño cerebral adquirido u otras patologías neuológicas.</p>
+   <p>Asesoramiento a familias</p>
 
-<h2>Metodología</h2>
-  <p>¿En que te ayudamos y como lo hacemos?</p>
-  <p>Enfermedad de Alzheimer y otras demencias</p>
-  <p>Enfermedad de Parkinson</p>
-  <p>Daño cerebral adquirido</p>
-  <p>Esclerosis múltiple</p>
+ <h2>Metodología</h2>
+   <p>¿En que te ayudamos y como lo hacemos?</p>
+   <p>Enfermedad de Alzheimer y otras demencias</p>
+   <p>Enfermedad de Parkinson</p>
+   <p>Daño cerebral adquirido</p>
+   <p>Esclerosis múltiple</p>
 
-          <Link href="/reservas?servicio=estimulacioncognitiva">
+    <Link href="/reservas?servicio=estimulacioncognitiva">
     <button className="boton-reservar">
       Reservar
     </button>
   </Link>
 <section id="contacto" style={{ marginTop: "40px" }}>
-                    <h2>Envíanos un mensaje</h2>
-                     <p>..................Mas info y reservas...................</p> 
-                    <ContactForm />
-       </section>
-     </div>
-    </main>
+  <h2>Envíanos un mensaje</h2>
+   <p>..................Mas info y reservas...................</p> 
+    <ContactForm />
+</section>
+</div>
+</main>
   );
 }
 
