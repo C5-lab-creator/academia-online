a0578072e8a86b691993a9d2364fb6b2834b1966
C5-lab-creator <sheylaapariciosuarez@gmail.com>
2026-08-08 13:41:03 +0200

    Actualizar robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/admin/", "/api/"],
+      disallow: ["/admin/", "/api/", "/aulavirtual/"],
     },
     sitemap: "https://academia-menteabierta.com/sitemap.xml",
   };
