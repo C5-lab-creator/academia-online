8cf32350195663ec2d43c7e8ca4aaf2741059010
C5-lab-creator <sheylaapariciosuarez@gmail.com>
2026-09-07 22:11:14 +0200

    modified:   app/adaptacioneshogar/page.tsx

--- app/adaptacioneshogar/page.tsx
+++ app/adaptacioneshogar/page.tsx
@@ -30,7 +30,7 @@ export default function Envejecimiento() {
 
               <Link href="/reservas?servicio=adaptacioneshogar">
                 <button className="bg-blue-900 hover:bg-blue-700 text-white px-8 py-4 rounded-xl font-semibold transition">
-                  Reservar una valoración
+                  Solicitar una valoración
                 </button>
               </Link>
 
